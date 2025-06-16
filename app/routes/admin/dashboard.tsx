c43b1a9f03acfb607e2d9c527bf2f0dddcf12f9e
Ivan López Rodríguez <ivaan100@gmail.com>
2025-06-16 22:05:14 +0200

    add: Sidebar and header components

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,5 +1,17 @@
+import { Header } from 'components'
+
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  const user = { name: 'Ivan' }
+
+  return (
+    <main className='dashboard wrapper'>
+      <Header
+        title={`Welcome ${user?.name ?? 'Guest'} 👋🏼`}
+        description='Track activity, trends and popular destinations in real time'
+      />
+      Dashboard Page Contents
+    </main>
+  )
 }
 
 export default Dashboard
