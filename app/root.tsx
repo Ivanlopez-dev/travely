949efcc3d72a8823260880a5e2cf16d6a011597b
Ivan López Rodríguez <ivaan100@gmail.com>
2025-06-21 17:20:28 +0200

    add: Favicon link rel

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,8 @@ export const links: Route.LinksFunction = () => [
   {
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
-  }
+  },
+  { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }
 ]
 
 import { registerLicense } from '@syncfusion/ej2-base'
